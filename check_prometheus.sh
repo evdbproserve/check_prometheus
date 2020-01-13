6ea2f26d015bf9dddf72a6c11eee7361700a0f93
Erwin van den Bovenkamp <evandenbovenkamp@proserve.nl>
2020-01-13 12:50:04 +0100

    Initial release

---
+++ check_prometheus.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+STATE_DEPENDENT=4
+
+HOSTNAME=
+PORT=
+CHECK=
+
+usage() {
+
+# Real men do not read the instructions....
+
+echo " "
+echo "$0 - check for Promeutheus"
+echo " "
+echo "$0 [options] -h [hostname/ip] -p [port]"
+echo " "
+echo "Option		Meaning"
+echo "-c		Check host before executing check"
+echo " "
+exit $STATE_DEPENDENT
+
+}
+
+check_req() {
+
+# Requirements to run this check.
+requirements=( curl nc )
+
+for check in "${requirements[@]}"
+do
+	[ $(which $check) ] || ( echo "Please install $check."; exit $STATE_DEPENDENT )
+done
+
+}
+
+check_host() {
+
+nc -zv -4 -w 1 $HOSTNAME $PORT > /dev/null 2>&1
+[ $? -gt 0 ] && ( echo "CRITICAL - Prometheus unavailable on $HOSTNAME using port $PORT"; exit $STATE_CRITICAL )
+
+
+}
+
+
+check_health() {
+
+# Ready check
+result=$(curl -k --connect-timeout 2 $HOSTNAME:$PORT/-/ready 2>/dev/null)
+[ $? -gt 0 ] && ( echo "CRITICAL - Prometheus unavailable on $HOSTNAME using port $PORT"; exit $STATE_CRITICAL )
+[[ $result == *"Ready"* ]] || ( echo "CRITICAL - Prometheus is NOT ready on $HOSTNAME using port $PORT"; exit $STATE_CRITICAL )
+
+# Health check
+result=$(curl -k --connect-timeout 2 $HOSTNAME:$PORT/-/healthy 2>/dev/null)
+[ $? -gt 0 ] && ( echo "CRITICAL - Prometheus unavailable on $HOSTNAME using port $PORT"; exit $STATE_CRITICAL )
+[[ $result == *"Healthy"* ]] || ( echo "CRITICAL - Prometheus is NOT healthy on $HOSTNAME using port $PORT"; exit $STATE_CRITICAL )
+
+
+}
+
+
+
+while getopts “h:p:c?” OPTION
+do
+     case $OPTION in
+         h)
+             HOSTNAME=$OPTARG
+             ;;
+         p)
+             PORT=$OPTARG
+             ;;
+         c)
+             CHECK=true
+             ;;
+         ?)
+             usage
+             ;;
+     esac
+done
+
+[ -z $PORT ] && usage
+[ -z $HOSTNAME ] && usage
+
+# Check if all required files are on the system
+check_req
+
+# Check if avaiability option is true
+[ $CHECK ] && check_host
+
+# Run the healty check(s)
+check_health
+
+# If you are here, all checks are good and we can leave it a good feeling.
+echo "OK - Prometheus is running like it should. All good!"
+exit $STATE_OK
